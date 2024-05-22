9e14c4bce64e0b283f98532ce1cf1cf65bb7f4fb
BlastTea <arya.jbr.999@gmail.com>
2024-05-22 11:43:25 +0700

    feat: add active to every details

--- components/nav.php
+++ components/nav.php
@@ -6,9 +6,9 @@ $current_page = basename($_SERVER['PHP_SELF']);
     <ul>
         <li><a href="index.php" class="<?= ($current_page == 'index.php') ? 'active' : ''; ?>">Home</a></li>
         <li><a href="about.php" class="<?= ($current_page == 'about.php') ? 'active' : ''; ?>">About</a></li>
-        <li><a href="services.php" class="<?= ($current_page == 'services.php') ? 'active' : ''; ?>">Services</a></li>
-        <li><a href="projects.php" class="<?= ($current_page == 'projects.php') ? 'active' : ''; ?>">Projects</a></li>
-        <li><a href="blog.php" class="<?= ($current_page == 'blog.php') ? 'active' : ''; ?>">Blog</a></li>
+        <li><a href="services.php" class="<?= ($current_page == 'services.php' or $current_page == 'service-details.php') ? 'active' : ''; ?>">Services</a></li>
+        <li><a href="projects.php" class="<?= ($current_page == 'projects.php' or $current_page == 'project-details.php') ? 'active' : ''; ?>">Projects</a></li>
+        <li><a href="blog.php" class="<?= ($current_page == 'blog.php' or $current_page == 'blog-details.php') ? 'active' : ''; ?>">Blog</a></li>
         <li class="dropdown"><a href="#"><span>Dropdown</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
             <ul>
                 <li><a href="#">Dropdown 1</a></li>
